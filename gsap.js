572da567a24d865259cb8510701bd79702ba9983
Nandhakumar63 <19201180+NandhaKumar63@users.noreply.github.com>
2024-02-01 01:54:57 +0530

    enabled animations

--- gsap.js
+++ gsap.js
@@ -1,29 +1,30 @@
 
 gsap.registerPlugin(ScrollTrigger);
+const tl = gsap.timeline();
 
-// const splitTypes = document.querySelectorAll('.index__section.about .paragraph .word .word__inner');
-// splitTypes.forEach((char) => {
-//   const text = new SplitType(char, {
-//     types: 'chars'
-//   });
+const splitTypes = document.querySelectorAll('.index__section.about .paragraph .word .word__inner');
+splitTypes.forEach((char) => {
+  const text = new SplitType(char, {
+    types: 'chars'
+  });
 
-//   gsap.from(text.chars, {
-//     backgroundPositionX: 0,
-//     ease: "none",
-//     stagger: 0.2,
-//     y: 0,
-//     opacity: 0,
-//     transformOrigin: 'top',
-//     duration: 3,
-//     scrollTrigger: {
-//       trigger: char,
-//       // markers: true,
-//       scrub: 0.6,
-//       start: "top 90%",
-//       end: "top 50%"
-//     }
-//   });
-// });
+  gsap.from(text.chars, {
+    backgroundPositionX: 0,
+    ease: "none",
+    stagger: 0.2,
+    y: 0,
+    opacity: 0,
+    transformOrigin: 'top',
+    duration: 3,
+    scrollTrigger: {
+      trigger: char,
+      // markers: true,
+      scrub: 0.6,
+      start: "top 90%",
+      end: "top 50%"
+    }
+  });
+});
 
 
 // const container = document.querySelector(".index__section.projects");
@@ -49,80 +50,80 @@ gsap.registerPlugin(ScrollTrigger);
 //   },
 // });
 
-// gsap.to(".projects__title__wrap", {
-//   x: () => -(document.querySelector(".projects__title__wrap").offsetWidth - window.innerWidth),
-//   ease: "sine.out",
-//   duration: 0.2,
-//   scrollTrigger: {
-//     trigger: ".projects__content",
-//     start: "top top",
-//     end: () => "+=" + document.querySelector(".projects__title__wrap").offsetWidth,
-//     scrub: true,
-//     pin: true,
-//     pinSpacing: false
-//   },
-// });
+gsap.to(".projects__title__wrap", {
+  x: () => -(document.querySelector(".projects__title__wrap").offsetWidth - window.innerWidth),
+  ease: "sine.out",
+  duration: 0.2,
+  scrollTrigger: {
+    trigger: ".projects__content",
+    start: "top top",
+    end: () => "+=" + document.querySelector(".projects__title__wrap").offsetWidth,
+    scrub: true,
+    pin: true,
+    pinSpacing: false
+  },
+});
 
 
 
-// gsap.set('.projects__item', {
-//   y: '100%'
-// });
+gsap.set('.projects__item', {
+  y: '100%'
+});
 
-// gsap.to('.projects__item', {
-//   y: 0,
-//   duration: 0.5,
-//   stagger: 0.2,
-//   ease: 'linear',
-//   scrollTrigger: {
-//     trigger: '.projects__wrap',
-//     start: 'top 70%',
-//     end: "+=500px",
-//     scrub: 0.5,
-//     // markers: true,
-//   }
-// })
+gsap.to('.projects__item', {
+  y: 0,
+  duration: 0.5,
+  stagger: 0.2,
+  ease: 'linear',
+  scrollTrigger: {
+    trigger: '.projects__wrap',
+    start: 'top 70%',
+    end: "+=500px",
+    scrub: 0.5,
+    // markers: true,
+  }
+})
 
 // // Project stack FX
 let allProjects = gsap.utils.toArray('.index__section.intro .title div');
 
 
 
-// gsap.fromTo(".index__section.intro .title div",
-//   {
-//     y: window.innerHeight
-//   }, {
-//   y: -50 + (allProjects.length - 1),
-//   ease: "sine.out",
-//   stagger: 0.5,
-//   duration: 1,
-//   scrollTrigger: {
-//     trigger: ".index__section.intro",
-//     start: () => "top 50px",
-//     scrub: 1,
-//     pin: true,
-//     pinSpacing: false,
-//     // markers: true
-//   }
-// });
+gsap.fromTo(".index__section.intro .title div",
+  {
+    y: window.innerHeight
+  }, {
+  y: -50 + (allProjects.length - 1),
+  ease: "sine.out",
+  stagger: 0.5,
+  duration: 1,
+  scrollTrigger: {
+    trigger: ".index__section.intro",
+    start: () => "top 50px",
+    scrub: 1,
+    pin: true,
+    pinSpacing: false,
+    // markers: true
+  }
+});
 
 
-// gsap.fromTo(".polygon_section.about_us",
-//   {
-//     x: '300px'
-//   }, {
-//   x: 0,
-//   duration: 0.2,
-//   scrollTrigger: {
-//     trigger: ".polygon_section.about_us",
-//     start: () => "top 60%",
-//     scrub: 0.2,
-//     // markers: true
-//   }
-// });
+gsap.fromTo(".polygon_section.about_us",
+  {
+    x: '300px'
+  }, {
+  x: 0,
+  duration: 0.2,
+  scrollTrigger: {
+    trigger: ".polygon_section.about_us",
+    start: () => "top 60%",
+    scrub: 0.2,
+    // markers: true
+  }
+});
 
 
-// const bannerCaptionSplit = document.querySelectorAll('.banner-caption');
+const bannerCaptionSplit = document.querySelectorAll('.banner-caption');
 
 gsap.fromTo(".logo", {
   scale: 0.9,
@@ -146,8 +147,6 @@ function menuClick(){
     }
   }
 }
-console.log(gsapScrollTrigger);
 if (/Mobi|Android/i.test(navigator.userAgent)) {
-  alert('disabled');
   gsap.core.globals("ScrollTrigger").disable();
 }
